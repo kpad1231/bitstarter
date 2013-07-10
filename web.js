e38e94a42153b4c8f538332a27afdfbd5fbb3bf2
John Smith <example@stanford.edu>
2013-07-10 23:07:07 +0000

    defined var fs

--- web.js
+++ web.js
@@ -2,11 +2,13 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+var infile = "index.html";
 var buffer = new Buffer(64);
 
 app.get('/', function(request, response) {
 //  response.send('Hello World2!');
-  buffer = fs.readfile('index.html');
+  buffer = fs.readfile(infile);
   response.send(buffer.toString());
 });
 
