7f7aa10ade60a4888c4a71493a545e7865eff155
John Smith <example@stanford.edu>
2013-07-10 22:50:17 +0000

    declared var buffer in web.js

--- web.js
+++ web.js
@@ -2,9 +2,12 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var buffer = new Buffer(64);
+
 app.get('/', function(request, response) {
 //  response.send('Hello World2!');
-  response.send(buf.toString(fs.readfile('index.html')));
+  buffer = fs.readfile('index.html');
+  response.send(buf.toString(buffer));
 });
 
 var port = process.env.PORT || 5000;
